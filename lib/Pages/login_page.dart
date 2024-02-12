dfb4f7fb72b2817861cc9b6c52fc82e04e1023a0
suhaila273 <suhailasalam273@gmail.com>
2024-02-12 11:05:02 +0530

    login updated

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -8,6 +8,17 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String email="";
+  String pass="";
+
+  TextEditingController email1=new TextEditingController();
+  TextEditingController pass1=new TextEditingController();
+
+  void SendValue(){
+    print("email :"+email1.text);
+    print("password : "+pass1.text);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +28,7 @@ class _LoginPageState extends State<LoginPage> {
           children: [
             SizedBox(height: 15,),
             TextField(
+              controller: email1,
               decoration: InputDecoration(
                 labelText: "Email id",
                 hintText: "Enter email id",
@@ -25,6 +37,7 @@ class _LoginPageState extends State<LoginPage> {
             ),
             SizedBox(height: 15,),
             TextField(
+              controller: pass1,
               decoration: InputDecoration(
                 labelText: "Password",
                 hintText: "enter valid password",
@@ -32,7 +45,7 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
             SizedBox(height: 15,),
-            ElevatedButton(onPressed: (){},
+            ElevatedButton(onPressed: SendValue,
                 child: Text("Login")),
             SizedBox(height: 15,),
             ElevatedButton(onPressed: (){},
